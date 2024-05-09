79d637e8f109fe4e254822c7ab97652ce8b956af
KeyboardguyBen <gillieauxbenjamin@gmail.com>
2024-05-09 19:47:40 +0200

    Travail sur building et buildingType

---
+++ DomeWars/Assets/Scripts/Building.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public int id;
+    private int Territory;
+    private BuildingType type;
+    private int earnings;
+    private int upKeep;
+    //private Dictionary<Upgrade, bool> relation = new Dictionary<Gang, int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DomeWars/Assets/Scripts/BuildingType.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingType : MonoBehaviour
+{
+    private int id;
+    private string name;
+    private string description;
+    private int effectStrength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
