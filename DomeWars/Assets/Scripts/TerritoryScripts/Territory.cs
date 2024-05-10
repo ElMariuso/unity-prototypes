3cbae9813c4316622d3ed0f85d1655c0a03fc0ef
ElMariuso <thiry.marius@gmail.com>
2024-05-10 02:50:57 +0200

    part 1

---
+++ DomeWars/Assets/Scripts/TerritoryScripts/Territory.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Territory : MonoBehaviour
+{
+    public int id;
+    public string territoryName;
+    public List<int> neighborgs;
+    // buildings
+    // private short danger = 0;
+    // units
+
+    /* Gang */
+    // public Gang gang;
+    private Color color;
+
+    /* Neighborgs */
+
+    /* Gang */
+}
